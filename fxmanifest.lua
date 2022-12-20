15c2a7a3d070088e5bde36711d863e23978caa1c
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2022-12-20 01:02:38 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "discord.gg/Nb23tC3wmA"
+version "3.1"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/*.css',
+    'img/*',
+    'img/*/*',
+    'js/*.js',
+    'js/snowStorm.js',
+    'js/snowStorm-min.js',
+    'main.lua',
+}
+loadscreen_manual_shutdown 'yes'
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
